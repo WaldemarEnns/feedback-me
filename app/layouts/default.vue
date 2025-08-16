92cbd834528aec4b242c895b77afd1fdc571afcf
WaldemarEnns <ennsdesigns96@gmail.com>
2025-08-16 20:33:17 +0200

    Creates default layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="min-h-screen grid grid-cols-[12rem_1fr]">
+        <!-- Left Sidebar - Navigation -->
+        <aside class="border-r border-gray-200 px-1">
+            <TheNavigation />
+        </aside>
+        
+        <!-- Right Side - Page Content -->
+        <main class="p-6">
+            <slot />
+        </main>
+    </div>
+</template>
