4c1107a1cd1fd480c86767210ab1057b982a1c6d
WaldemarEnns <ennsdesigns96@gmail.com>
2025-08-16 20:33:12 +0200

    Creates basic navigation component

---
+++ app/components/TheNavigation.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const items = [
+    {
+        label: 'Home',
+        to: '/',
+        icon: 'i-heroicons-home',
+    },
+    
+]
+</script>
+
+<template>
+    <UNavigationMenu
+        orientation="vertical"
+        class="w-full"
+        :items="items"
+    ></UNavigationMenu>
+</template>
