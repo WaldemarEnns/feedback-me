c1921f0064eb6540e8bdd9d91455349126fa60fe
WaldemarEnns <ennsdesigns96@gmail.com>
2025-08-18 18:09:12 +0200

    Adds receipients page

---
+++ app/pages/receipients.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+const receipients = ref([
+    {
+        id: 1,
+        name: 'John Doe',
+        description: 'John Doe is a software engineer at Google',
+        feedbackCount: 10,
+    },
+    
+    {
+        id: 2,
+        name: 'Jane Doe',
+        description: 'Jane Doe is a software engineer at Facebook',
+        feedbackCount: 10,
+    },
+    
+    {
+        id: 3,
+        name: 'John Smith',
+        description: 'John Smith is a software engineer at Apple',
+        feedbackCount: 10,
+    },
+    
+])
+</script>
+
+<template>
+    <div>
+        <h1 class="text-2xl font-bold">Receipients</h1>
+        <UTable
+            :data="receipients"
+        >
+            <template #feedbackCount-header>
+                <span>Feedback given</span>
+            </template>
+            <template #feedbackCount-cell="{ row }">
+                <UBadge>{{ row.original.feedbackCount }}</UBadge>
+            </template>
+        </UTable>
+    </div>
+</template>
